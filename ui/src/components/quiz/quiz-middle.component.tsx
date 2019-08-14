69527e138f89a42589e5e81485462313752b0cef
carichka <carichka@yahoo.com>
2019-08-14 19:51:46 -0400

    Added quiz form layout

--- ui/src/components/quiz/quiz-middle.component.tsx
+++ ui/src/components/quiz/quiz-middle.component.tsx
@@ -1,11 +1,52 @@
 import React, { Component } from 'react';
+import {
+    Card, Button, CardTitle, CardText,
+    Container, Row, Col
+  } from 'reactstrap';
 
 export default class QuizMiddleComponent extends Component{
     render() {
         return (
-            <div>
-                <h1>Quiz Middle Component</h1>
-            </div>
-        )
-    }
-}
+            <Container>
+                <h6>Quiz Middle Component</h6>
+                <div>
+                   <Card body inverse color="primary">
+                    <h1><CardTitle>Question</CardTitle></h1>
+                    <h3><CardText>What do we call a primary key that consists of multiple columns in a table?</CardText></h3>
+                  </Card> 
+                  <br></br>
+                <Row>
+                    <Col>
+                  <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
+                    <CardText>A composite key</CardText>
+                    <Button>Button</Button>
+                  </Card>
+                  </Col>
+                  <Col>
+                  <Card body inverse color="success">
+                    <CardText>A primary key</CardText>
+                    <Button color="secondary">Button</Button>
+                  </Card>
+                  </Col>
+                  </Row>
+                      <br></br>
+                  <Row>
+                      
+                    <Col>
+                  <Card body inverse color="info">
+                    <CardText>A foreign key</CardText>
+                    <Button color="secondary">Button</Button>
+                  </Card>
+                  </Col>
+                  <Col>
+                  <Card body inverse color="warning">
+                    <CardText>A friendly key</CardText>
+                    <Button color="secondary">Button</Button>
+                  </Card>
+                  </Col>
+                  </Row>
+                </div>
+                </Container>
+              );
+            };
+        }
