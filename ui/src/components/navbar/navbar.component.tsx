e1e9b4e5d12aa5658a5c2472d8b9b8e94b3ec3c7
JordanLandberg <jordanlandberg@gmail.com>
2019-08-14 20:33:02 -0400

    Completed navbar with basic components

--- ui/src/components/navbar/navbar.component.tsx
+++ ui/src/components/navbar/navbar.component.tsx
@@ -6,7 +6,7 @@ export default class NavbarComponent extends Component {
         return (
             <nav className="navbar navbar-toggleable-md navbar-expand-lg navbar-light bg-light display-front nav-pad">
                 <div className="navbar-header c-pointer shift-left">
-                    <Link to="/home" className="unset-anchor">Logo Image</Link>
+                    <Link to="/home/login" className="unset-anchor">Logo Image</Link>
                 </div>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -19,7 +19,24 @@ export default class NavbarComponent extends Component {
                         <li className="nav-item active dropdown">
                             <div className="nav-link dropdown-toggle pointer" id="examples-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Categories</div>
                             <div className="dropdown-menu" aria-labelledby="examples-dropdown">
-                                <div className="dropdown-item"><Link to="/movies" className="unset-anchor nav-link active">Movies</Link></div>
+                                <div className="dropdown-item"><h6>Front-End</h6></div>
+                                <div className="dropdown-divider"></div>
+                                <div className="dropdown-item"><Link to="/admin/review" className="unset-anchor nav-link active">HTML</Link></div>
+                                <div className="dropdown-item"><Link to="/admin/review" className="unset-anchor nav-link active">CSS</Link></div>
+                                <div className="dropdown-item"><Link to="/admin/review" className="unset-anchor nav-link active">JavaScript</Link></div>
+                                <div className="dropdown-item"><Link to="/admin/review" className="unset-anchor nav-link active">React</Link></div>
+                                <div className="dropdown-item"><Link to="/admin/review" className="unset-anchor nav-link active">Redux</Link></div>
+                                <div className="dropdown-divider"></div>
+                                <div className="dropdown-item"><h6>Back-End</h6></div>
+                                <div className="dropdown-divider"></div>
+                                <div className="dropdown-item"><Link to="/admin/review" className="unset-anchor nav-link active">Node</Link></div>
+                                <div className="dropdown-item"><Link to="/admin/review" className="unset-anchor nav-link active">Express</Link></div>
+                                <div className="dropdown-divider"></div>
+                                <div className="dropdown-item"><h6>Data Structures/Algorithms</h6></div>
+                                <div className="dropdown-divider"></div>
+                                <div className="dropdown-item"><Link to="/admin/review" className="unset-anchor nav-link active">Data Structures/Algorithms</Link></div>
+                                <div className="dropdown-divider"></div>
+                                <div className="dropdown-item"><Link to="/admin/review" className="unset-anchor nav-link active">All Categories</Link></div>
                             </div>
                         </li>
                         <li className="nav-item active dropdown">
@@ -40,6 +57,6 @@ export default class NavbarComponent extends Component {
                     </ul>
                 </div>
             </nav>
-        )
-    }
-}
+                        )
+                    }
+                }
