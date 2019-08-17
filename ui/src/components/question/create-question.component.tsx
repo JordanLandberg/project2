935dcbb4eec92f27db5408f22a93c30628750626
this-is-yosuke <yosuke.kibe@gmail.com>
2019-08-17 17:51:49 -0400

    Made a question creation form

--- ui/src/components/question/create-question.component.tsx
+++ ui/src/components/question/create-question.component.tsx
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
-export default class CreateQuestionComponent extends Component{
+export default class CreateQuestionComponent extends React.Component{
     render() {
         return (
-            <div>
-                <h1>Create Question Component</h1>
-            </div>
+            <Form>
+        <FormGroup>
+          <Label for="exampleEmail">Question</Label>
+          <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="examplePassword">Correct Answer</Label>
+          <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="examplePassword">Incorrect Answer #1</Label>
+          <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="examplePassword">Incorrect Answer #2</Label>
+          <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="examplePassword">Incorrect Answer #3</Label>
+          <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+        </FormGroup>
+        </Form>
         )
     }
 }
