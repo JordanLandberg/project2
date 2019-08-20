c9ac1b706db8ef7fb2f21d1b91756aac6f6964cc
JordanLandberg <jordanlandberg@gmail.com>
2019-08-19 20:27:42 -0400

    updated categories page and css for that page

--- ui/src/components/categories/categories.component.tsx
+++ ui/src/components/categories/categories.component.tsx
@@ -1,12 +1,52 @@
 import React, { Component } from 'react';
-
+import { Card, CardTitle, Col, Container, Row } from 'reactstrap';
 export default class CategoriesComponent extends Component {
-
+    // this.props.history.push('/quiz/start')
     render() {
         return (
-            <div>
-                <h1>Categories</h1>
-            </div>
+            <Container>
+                <h1>Front-End</h1>
+                <Row>
+                    <Col>
+                        <Card className="answer1 clickable"
+                            // onClick={()}
+                            body inverse>
+                            <h1><CardTitle>CSS</CardTitle></h1>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card className="answer2 clickable" body inverse>
+                            <h1><CardTitle>HTML</CardTitle></h1>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card className="answer3 clickable" body inverse>
+                            <h1><CardTitle>JavaScript</CardTitle></h1>
+                        </Card>
+                    </Col>
+                </Row>
+                <h1>Back-End</h1>
+                <Row>
+                    <Col>
+                        <Card className="answer4 clickable" body inverse>
+                            <h1><CardTitle>Node</CardTitle></h1>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card className="answer5 clickable" body inverse>
+                            <h1><CardTitle>Express</CardTitle></h1>
+                        </Card>
+                    </Col>
+                </Row>
+                <h1>Data Structures / Algorithms</h1>
+                <Row>
+                    <Col>
+                        <Card className="question clickable" body inverse>
+                            <h1><CardTitle>Data Structures / Algorithms</CardTitle></h1>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         )
     }
 }
